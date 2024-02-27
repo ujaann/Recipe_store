b25c06919e15de85a8b5361b675067476d7ffc13
ujaann <ujanst@gmail.com>
2024-02-27 23:18:53 +0545

    Re-initialize backend

---
+++ recipe_backend/src/main/java/com/example/recipe_backend/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.recipe_backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+    @GetMapping("/")
+    public String helloUser(){
+        return "User Level Access";
+    }
+}
